f2a43cf0a6b3ce67a86f0032a6e5b3992d7af3d9
dangeus <bjokugan@mail.ru>
2025-01-31 21:36:35 +0300

    добавил описание задания

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Создайте анонимный модуль в данном файле, который принимает имя папки и возвращает абсолютный путь к ней
 const getDir = require('./caller-dir/modules/path-resolve')
 
 console.log(getDir())
